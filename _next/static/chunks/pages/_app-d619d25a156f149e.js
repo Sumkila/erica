3d341732d9c7017b3c800564772c963b302e4fdd
Sumkila <108103544+Sumkila@users.noreply.github.com>
2026-03-14 04:31:04 +0200

    Add files via upload

---
+++ _next/static/chunks/pages/_app-d619d25a156f149e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{4945:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(7415)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(4945),n(1439))),_N_E=_.O()}]);
